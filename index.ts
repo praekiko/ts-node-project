e68acdb778ff02a8240ff9afb5eefe2f648c73e7
Lalitwadee Damyos <rainbow.prae@gmail.com>
2021-05-10 17:17:23 +0700

    fetch api, rollup, login middleware

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
 import express from "express";
+import axios from "axios";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.get("/users", async (req, res) => {
+  const {
+    data: { results },
+  } = await axios.get("https://randomuser.me/api/");
+  res.json(results);
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-export function sum(a: number, b: number) {
-  return a + b;
-}
---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+  input: "index.ts",
+  output: [
+    {
+      file: "./build/index.js",
+      format: "cjs",
+    },
+  ],
+};
