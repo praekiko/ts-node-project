10633704704e4d45c25fdb4c5df1017378a02519
Lalitwadee Damyos <rainbow.prae@gmail.com>
2021-05-10 15:25:25 +0700

    add express & typescript

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
